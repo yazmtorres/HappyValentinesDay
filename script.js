b5e3239360db671aa7fe779b04aa18abe2b2d576
yazmtorres <yazmartineztorres@gmail.com>
2025-02-21 02:00:17 -0500

    updated script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 function changeImage() {
     let image = document.getElementById("dynamicImage");
-
     if (image.src.includes("37b37ffccfd401a85ffc42a2b1eccaad.jpg")) {
-        image.src = "https://via.placeholder.com/200"; // Change to a placeholder
+        image.src = "https://i.pinimg.com/736x/5a/7f/3d/5a7f3d7ccbd5a2c2b3ee9f6e09f7b4b3.jpg"; // New Valentine’s Day Image
     } else {
-        image.src = "https://i.pinimg.com/736x/37/b3/7f/37b37ffccfd401a85ffc42a2b1eccaad.jpg"; // Revert to your image
+        image.src = "https://i.pinimg.com/736x/37/b3/7f/37b37ffccfd401a85ffc42a2b1eccaad.jpg"; // Original image
     }
 }
